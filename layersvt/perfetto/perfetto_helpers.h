13c89fb4fadefb119e4cb2d0ea9334c3ba65780e
Mary Koliadenko <mako@google.com>
2026-04-16 15:40:46 +0000

    Revert changes to perfetto_helpers to focus on screenshots

--- layersvt/perfetto/perfetto_helpers.h
+++ layersvt/perfetto/perfetto_helpers.h
@@ -11,8 +11,7 @@ PERFETTO_DEFINE_CATEGORIES(
     perfetto::Category("VulkanCPUTiming/VkDevice").SetDescription("Device Level Commands"),
     perfetto::Category("VulkanCPUTiming/VkQueue").SetDescription("Queue Level Commands"),
     perfetto::Category("VulkanCPUTiming/VkCommandBuffer").SetDescription("Command Buffer Level Commands"),
-    perfetto::Category("VulkanCPUTiming/Other").SetDescription("Other Commands"),
-    perfetto::Category("application_screenshot").SetDescription("Vulkan Layer Screenshots")
+    perfetto::Category("VulkanCPUTiming/Other").SetDescription("Other Commands")
 );
 
 // Enum matching the Perfetto categories above.
@@ -25,21 +24,6 @@ enum class CPUTimingCategory {
     VkQueue,
     VkCommandBuffer,
     Other
-};
-
-class ScreenshotDataSource : public perfetto::DataSource<ScreenshotDataSource> {
- public:
-  void OnSetup(const SetupArgs&) override {}
-  void OnStart(const StartArgs&) override {
-#ifdef ANDROID
-    __android_log_print(ANDROID_LOG_INFO, "screenshot", "ScreenshotDataSource::OnStart called");
-#endif
-  }
-  void OnStop(const StopArgs&) override {
-#ifdef ANDROID
-    __android_log_print(ANDROID_LOG_INFO, "screenshot", "ScreenshotDataSource::OnStop called");
-#endif
-  }
 };
 
 void InitializePerfetto();
