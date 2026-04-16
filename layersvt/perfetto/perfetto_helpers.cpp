269aa4c67e5155b58a271426094f943c349f3033
Mary Koliadenko <mako@google.com>
2026-04-16 12:47:42 +0000

    Fix unresolved external symbol for TrackEventDataSource on Windows.

--- layersvt/perfetto/perfetto_helpers.cpp
+++ layersvt/perfetto/perfetto_helpers.cpp
@@ -4,6 +4,18 @@
 
 PERFETTO_TRACK_EVENT_STATIC_STORAGE();
 
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN) && !PERFETTO_BUILDFLAG(PERFETTO_COMPILER_GCC)
+namespace perfetto {
+// Workaround for a linker error on Windows where the static member definition
+// for TrackEventDataSource::type() is missing due to an empty macro in the amalgamated header.
+template <>
+perfetto::internal::DataSourceType& DataSourceHelper<perfetto::internal::TrackEventDataSource, perfetto::internal::TrackEventDataSourceTraits>::type() {
+    static perfetto::internal::DataSourceType type_;
+    return type_;
+}
+} // namespace perfetto
+#endif // PERFETTO_BUILDFLAG(PERFETTO_OS_WIN) && !PERFETTO_BUILDFLAG(PERFETTO_COMPILER_GCC)
+
 static std::once_flag perfetto_init_once;
 
 void InitializePerfetto() {
